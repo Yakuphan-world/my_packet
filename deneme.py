58946a0e4297be7d1cdcb31c3132a302d3332617
Yakup Han <107556647+Yakuphan-world@users.noreply.github.com>
2022-11-19 23:15:45 +0100

    yeni commit

---
+++ deneme.py
@@ -0,0 +1,4 @@
+import my_module as mym
+#my_module.py isimli modülü mym olarak kkisattim
+mym.my_function()
+print(mym.my_variable)
---
+++ my_module.py
@@ -0,0 +1,8 @@
+"""
+ bu program modül olusturma örnegidir
+"""
+import math
+my_variable = math.pi 
+def my_function():
+      print("beu benim fonksiyonum")
+
